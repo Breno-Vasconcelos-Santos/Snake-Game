b8acc09990ef228a1330db58858905b4687fcd1c
Breno <vasconcelos.bren456@gmail.com>
2024-12-03 08:14:20 -0300

    corrigindo espaçamento da borda

--- display/funcoes.py
+++ display/funcoes.py
@@ -1,8 +1,14 @@
-def desenhar_largura(n):
-    print("#" * n)
+def desenhar_largura(largura):
+    print("#" * largura)
 
-def desenhar_altura(n):
-    if n > 2: 
-        teste = n - 2
-        for i in range(teste):
-            print("#" + " " * teste + "#")
+def desenhar_altura(altura, largura):
+    if altura > 2:  
+        # desenha a parte superior da borda
+        desenhar_largura(largura)
+        
+        # desenha as laterais da borda
+        for i in range(altura - 2):
+            print("#" + " " * (largura - 2) + "#")
+        
+        # desenha a parte inferior da borda
+        desenhar_largura(largura)
--- display/teste.py
+++ display/teste.py
@@ -1,5 +1,3 @@
 from funcoes import *
 
-desenhar_largura(20)
-desenhar_altura(20)
-desenhar_largura(20)
+desenhar_altura(10, 60)
