d33ebda37386362ee35337d0d29ec85bd86e9312
Breno <vasconcelos.bren456@gmail.com>
2024-12-04 08:59:32 -0300

    organizando menu

---
+++ menu/funcoesMenu.py
@@ -0,0 +1,14 @@
+import os
+#import cursor
+#import Wconio2
+#import InquirerPy
+
+def menu():
+    print("Seja bem vindo(a) ao Snake-Game! ")
+    opcao=input("Digite 1 para jogar: ")
+    #if (opcao==1):
+        
+    #else:
+        #print("Opção indisponível! ")
+
+menu()
---
+++ menu/main.py
@@ -0,0 +1,7 @@
+from funcoesMenu import *
+
+menu()
+
+
+
+    
