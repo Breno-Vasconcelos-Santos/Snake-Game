925291943f34e9dadac7d92181d75106edc205e4
Breno <vasconcelos.bren456@gmail.com>
2025-01-14 18:01:55 -0300

    inicio das funções que serao utilizadas no jogo

---
+++ jogo/funcoesJogo.py
@@ -0,0 +1,5 @@
+#parte comentada pois ainda terá modificações a fazer
+#def gerar_comida():
+    #comida_x=random.randrange(0, largura) 
+    #comida_y=random.randrange(0, altura)
+    #return comida_x, comida_y
