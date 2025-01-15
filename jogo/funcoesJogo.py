985eacbfd736d9c5876c95b35f8d066d96d423aa
Breno <vasconcelos.bren456@gmail.com>
2025-01-15 14:36:26 -0300

    definindo função que irá gerar a cobra

--- jogo/funcoesJogo.py
+++ jogo/funcoesJogo.py
@@ -2,8 +2,7 @@
 import WConio2
 import curses
 
-altura = 50
-largura = 50
+altura , largura=50
 cobra = [(10, 10), (9, 10), (8, 10)] #valores testes
 #parte comentada pois ainda terá modificações a fazer
 def gerar_comida():
@@ -12,13 +11,15 @@ def gerar_comida():
     #return comida_x, comida_y
     pass
 
-def gerar_cobra():
+def gerar_cobra(stdscr, cobra):
     for i, (x, y) in enumerate(cobra): 
-        #stdscr.move(y, x)
+        stdscr.move(y, x)
         if i == 0:
-            WConio2.addstr("o")  
+            stdscr.addstr("o")  
         else:
-            WConio2.addstr("=")  
-    #stdscr.refresh()
+            stdscr.addstr("=")  
+    stdscr.refresh()
+
+
 
 #controle de teclas
