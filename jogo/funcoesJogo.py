a070a01e0a87dfc464894a5ca241176639e4f40a
Breno <vasconcelos.bren456@gmail.com>
2025-01-15 09:07:22 -0300

    adicionando futuros metodos a serem feitos

--- jogo/funcoesJogo.py
+++ jogo/funcoesJogo.py
@@ -1,5 +1,18 @@
+import random
+import WConio2
+import curses
+
+altura = 50
+largura = 50
+
 #parte comentada pois ainda terá modificações a fazer
-#def gerar_comida():
+def gerar_comida():
     #comida_x=random.randrange(0, largura) 
     #comida_y=random.randrange(0, altura)
     #return comida_x, comida_y
+    pass
+
+def gerar_cobra():
+    pass
+
+#controle de teclas
