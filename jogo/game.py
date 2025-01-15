0cea2b7ee785bcfe82e90f8eb185a0a0b94c5b31
Breno <vasconcelos.bren456@gmail.com>
2025-01-15 14:37:28 -0300

    introdução ao código principal

---
+++ jogo/game.py
@@ -0,0 +1,31 @@
+from funcoesJogo import *
+from display.funcoesDisplay import *
+
+def main(stdscr):
+    curses.curs_set(0) 
+    stdscr.timeout(100)
+    altura, largura = 50
+    cobra = [(10, 10), (9, 10), (8, 10)]
+    direcao = 454 #terminal não estava reconhecendo as teclas, tive que definir assim para reconhece-las, depois ver se tem como melhorar
+    opostos = {450: 456, 456: 450, 452: 454, 454: 452}
+
+    #desenho da borda do jogo
+    desenhar_altura(stdscr, altura, largura)
+
+    while True:
+        key = stdscr.getch() #comando de entrada do usuario
+        if key in opostos and opostos[direcao] != key:
+            direcao = key
+
+        y, x = cobra[0]
+        if direcao == 456:  #comando de direção para baixo
+            y += 1
+        elif direcao == 450:  #comando de direção para cima
+            y -= 1
+        elif direcao == 452:  #comando de direção para esquerda
+            x -= 1
+        elif direcao == 454:  # comando de direção para direita
+            x += 1
+
+
+
