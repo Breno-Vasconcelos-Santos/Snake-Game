de72f4fbb6f999a77ece2e9f770765238143c4b7
Breno <vasconcelos.bren456@gmail.com>
2025-01-16 19:19:35 -0300

    detecção de colisao da cobra consigo mesma ou com as bordas

--- jogo/game.py
+++ jogo/game.py
@@ -1,10 +1,12 @@
+import time
 from funcoesJogo import *
-from display.funcoesDisplay import *
+from  display.funcoesDisplay import *
+
 
 def main(stdscr):
     curses.curs_set(0) 
     stdscr.timeout(100)
-    altura, largura = 50
+    largura, altura = 30, 15
     cobra = [(10, 10), (9, 10), (8, 10)]
     direcao = 454 #terminal não estava reconhecendo as teclas, tive que definir assim para reconhece-las, depois ver se tem como melhorar
     opostos = {450: 456, 456: 450, 452: 454, 454: 452}
@@ -18,14 +20,33 @@ def main(stdscr):
             direcao = key
 
         y, x = cobra[0]
-        if direcao == 456:  #comando de direção para baixo
+        if direcao == 456:  #verifica se é o comando de direção para baixo
             y += 1
-        elif direcao == 450:  #comando de direção para cima
+        elif direcao == 450:  #verifica se é o comando de direção para cima
             y -= 1
-        elif direcao == 452:  #comando de direção para esquerda
+        elif direcao == 452:  #verifica se é o comando de direção para esquerda
             x -= 1
-        elif direcao == 454:  # comando de direção para direita
+        elif direcao == 454:  #verifica se é o comando de direção para direita
             x += 1
 
+        nova_cabeca = (y, x) #nova posição da cabeça
 
+        if (y <= 0 or y >= altura - 1 or x <= 0 or x >= largura - 1 or nova_cabeca in cobra):
+            break #verifica se colidiu com si mesmo ou com as bordas
+        
+        cobra.insert(0, nova_cabeca) #insersão da cabeça agora
+        
+        cobra.pop()
+        
+        stdscr.clear() #limpa a tela
+        desenhar_altura(stdscr, altura, largura) #redesenho da borda
+        gerar_cobra(stdscr, cobra) #redesenho da cobra
+        
+        time.sleep(0.1)
 
+    stdscr.clear()
+    stdscr.addstr(altura // 2, largura // 2 - 5, "Game Over!")
+    stdscr.refresh()
+    stdscr.getch()
+
+curses.wrapper(main)
