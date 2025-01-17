3e630046461df30788457ddd006e6915cfcf4bed
Bernardo <bernardosalvador2005@gmail.com>
2025-01-17 10:51:07 -0300

    Alterando caracteres de cabeça da cobra de acordo com a direção, passandomais um parâmetro

--- jogo/funcoesJogo.py
+++ jogo/funcoesJogo.py
@@ -13,8 +13,27 @@ def gerar_cobra(stdscr, cobra):
     for i, (y, x) in enumerate(cobra): 
         stdscr.move(y, x)
         if i == 0:
-            stdscr.addstr("o")  
+            stdscr.addstr("O")  
         else:
-            stdscr.addstr("=")  
+            stdscr.addstr("x")  
     stdscr.refresh()
 #controle de teclas
+
+def gerar_cobra(stdscr, cobra, direcao):
+    for i, (y, x) in enumerate(cobra): 
+        stdscr.move(y, x)
+        
+        # Se for a cabeça da cobra (primeiro elemento), desenha o caractere conforme a direção
+        if i == 0:
+            if direcao == 115:  # Para cima
+                stdscr.addstr("^")
+            elif direcao == 119:  # Para baixo
+                stdscr.addstr("v")
+            elif direcao == 97:  # Para esquerda
+                stdscr.addstr(">")
+            elif direcao == 100:  # Para direita
+                stdscr.addstr("<")
+        else:
+            stdscr.addstr("x")  # Corpo da cobra (todos os outros segmentos)
+
+    stdscr.refresh()
