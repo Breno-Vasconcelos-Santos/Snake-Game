65c1cf94da76771abb89c42866a7c51065db3231
Jamille <yamadajami@gmail.com>
2025-01-20 08:50:43 -0300

    organizando para comida aparecer aleatoriamente

--- jogo/game.py
+++ jogo/game.py
@@ -2,28 +2,23 @@ import time
 from funcoesJogo import *
 from  display.funcoesDisplay import *
 
-
 def main(stdscr):
-    curses.curs_set(0) 
+    curses.curs_set(0)
     stdscr.timeout(100)
     largura, altura = 30, 15
     cobra = [(10, 10), (9, 10), (8, 10)]
     direcao = 100 #terminal não estava reconhecendo as teclas, tive que definir assim para reconhece-las, depois ver se tem como melhorar
     opostos = {119: 115, 115: 119, 97: 100, 100: 97}
 
-    #desenho da borda do jogo
-    desenhar_altura(stdscr, altura, largura)
+    comida = gerar_comida(altura, largura, cobra)
 
-    #gera a comida
-    gerar_comida(altura, largura, cobra)
-
-    #desenha a comida na tela
-    desenhar_comida(stdscr, comida)
+    desenhar_altura(stdscr, altura, largura) #desenho da borda do jogo
 
     while True:
         key = stdscr.getch() #comando de entrada do usuario
         if key in opostos and opostos[direcao] != key:
             direcao = key
+
 
         y, x = cobra[0]
         if direcao == 115:  #verifica se é o comando de direção para baixo
@@ -35,24 +30,33 @@ def main(stdscr):
         elif direcao == 100:  #verifica se é o comando de direção para direita
             x += 1
 
+
         nova_cabeca = (y, x) #nova posição da cabeça
+
 
         if (y <= 0 or y >= altura - 1 or x <= 0 or x >= largura - 1 or nova_cabeca in cobra):
             break #verifica se colidiu com si mesmo ou com as bordas
-        
+       
         cobra.insert(0, nova_cabeca) #insersão da cabeça agora
-        
-        cobra.pop()
-        
+       
+        if nova_cabeca == comida:
+            comida = gerar_comida(altura, largura, cobra)  # Gera nova comida
+        else:
+            cobra.pop()
+       
         stdscr.clear() #limpa a tela
         desenhar_altura(stdscr, altura, largura) #redesenho da borda
         gerar_cobra(stdscr, cobra, direcao) #redesenho da cobra
-        
+        desenhar_comida(stdscr, comida)
+        stdscr.refresh()
+       
         time.sleep(0.1)
+
 
     stdscr.clear()
     stdscr.addstr(altura // 2, largura // 2 - 5, "Game Over!")
     stdscr.refresh()
     stdscr.getch()
+
 
 curses.wrapper(main)
