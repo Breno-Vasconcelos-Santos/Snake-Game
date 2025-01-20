654fd3fd0163cd03ba14699da1b54df06fd8c1df
gunvieira <gustavovieira132019@gmail.com>
2025-01-20 09:21:24 -0300

    adicionando main neste arquivo

--- jogo/funcoesJogo.py
+++ jogo/funcoesJogo.py
@@ -1,8 +1,9 @@
-#import random
+import random
 import curses
+from  display.funcoesDisplay import *
+import time
 
-cobra = [(10, 10), (9, 10), (8, 10)] #valores testes
-#parte comentada pois ainda terá modificações a fazer
+cobra = [(10, 10), (9, 10), (8, 10)] 
 def gerar_comida(altura, largura, cobra):
     while True:
         comida_y = random.randint(1, altura - 2)
@@ -33,3 +34,60 @@ def gerar_cobra(stdscr, cobra, direcao):
             stdscr.addstr("x")  # Corpo da cobra (todos os outros segmentos)
 
     stdscr.refresh()
+
+def main(stdscr):
+    contador = 0
+    curses.curs_set(0)
+    stdscr.timeout(100)
+    largura, altura = 30, 15
+    cobra = [(10, 10), (9, 10), (8, 10)]
+    direcao = 100 #terminal não estava reconhecendo as teclas, tive que definir assim para reconhece-las, depois ver se tem como melhorar
+    opostos = {119: 115, 115: 119, 97: 100, 100: 97}
+
+    comida = gerar_comida(altura, largura, cobra)
+    #desenho da borda do jogo
+    desenhar_altura(stdscr, altura, largura)
+
+    while True:
+        key = stdscr.getch()  # Recebe a entrada do usuário
+        if key in opostos and opostos[direcao] != key:
+            direcao = key
+
+        y, x = cobra[0]
+        if direcao == 115:  #verifica se é o comando de direção para baixo
+            y += 1
+        elif direcao == 119:  #verifica se é o comando de direção para cima
+            y -= 1
+        elif direcao == 97:  #verifica se é o comando de direção para esquerda
+            x -= 1
+        elif direcao == 100:  #verifica se é o comando de direção para direita
+            x += 1
+
+        nova_cabeca = (y, x) #nova posição da cabeça
+
+        if (y <= 0 or y >= altura - 1 or x <= 0 or x >= largura - 1 or nova_cabeca in cobra):
+            break  # Colisão com as bordas ou com a própria cobra
+
+        cobra.insert(0, nova_cabeca)
+
+        if nova_cabeca == comida:
+            comida = gerar_comida(altura, largura, cobra) # Gera nova comida
+            contador += 1
+        else:
+            cobra.pop()
+
+        stdscr.clear() #limpa a tela
+        desenhar_altura(stdscr, altura, largura) #redesenho da borda
+        gerar_cobra(stdscr, cobra, direcao) #redesenho da cobra
+        desenhar_comida(stdscr, comida)
+        stdscr.addstr(16, 1, f"Sua pontuação: {contador}")
+        stdscr.refresh()
+        time.sleep(0.1)
+
+    
+    stdscr.clear()
+    stdscr.refresh()
+
+    return contador  
+    
+
